cde326057cbdff3eda02d7818712e96249ce41a7
Евгений Терешкин <j.teresckhin@gmail.com>
2025-02-12 16:29:13 +0100

    Added changing colors when link to another page

--- src/pages/Hometasks/HometaskStudent.js
+++ src/pages/Hometasks/HometaskStudent.js
@@ -1,5 +1,16 @@
+import React from "react";
+import Navbar from "../../layouts/Navbar";
+
 const HometaskStudent = () => {
-  return <h1>Hometask Student</h1>;
+  return (
+    <Navbar>
+      <main>
+        <div className="p-6">
+          <h1 className="text-2xl font-bold">Hometask Student</h1>
+        </div>
+      </main>
+    </Navbar>
+  );
 };
 
 export default HometaskStudent;
