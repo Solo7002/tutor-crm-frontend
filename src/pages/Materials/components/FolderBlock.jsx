5517e2e0481c76ddbd4f4dab3fdfc1a81f317509
NikitaShyshkov <nikitasiskov737@gmail.com>
2025-02-18 15:03:05 +0200

    Настройка API-запросов для загрузки материалов

--- src/pages/Materials/components/FolderBlock.jsx
+++ src/pages/Materials/components/FolderBlock.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export default function FolderBlock({ name }) {
+export default function FolderBlock({ name, onClick }) {
     return (
-        <div className="w-[244px] h-[234px] relative bg-white rounded-3xl border border-[#d7d7d7]">
+        <div className="w-[244px] h-[234px] relative bg-white rounded-3xl border border-[#d7d7d7]" onClick={onClick}>
             <div data-svg-wrapper className="left-[72px] top-[40px] absolute">
                 <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M20.8333 16.6667H37.5L50 29.1667H79.1667C81.3768 29.1667 83.4964 30.0446 85.0592 31.6074C86.622 33.1702 87.5 35.2899 87.5 37.5V70.8333C87.5 73.0435 86.622 75.1631 85.0592 76.7259C83.4964 78.2887 81.3768 79.1667 79.1667 79.1667H20.8333C18.6232 79.1667 16.5036 78.2887 14.9408 76.7259C13.378 75.1631 12.5 73.0435 12.5 70.8333V25C12.5 22.7899 13.378 20.6702 14.9408 19.1074C16.5036 17.5446 18.6232 16.6667 20.8333 16.6667Z" stroke="#827FAE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
